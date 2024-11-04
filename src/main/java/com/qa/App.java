a6d8ce5263a257be41c1b93faa4d17c768ca7e4d
WilliamOstler <wostler@live.co.uk>
2024-11-04 15:00:07 +0000

    23

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Ch54arlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Wiqlliam"));
         System.out.println(sayGoodbye());
